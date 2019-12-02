305d04db194148ec474fdcde05e0285c3d8a5469
pi <pi_bartholdy@hotmail.com>
2019-12-02 20:12:34 +0100

    week 1

--- week1/Worksheet.py
+++ week1/Worksheet.py
@@ -1,6 +1,47 @@
-#Question 1: Write a line of code that will print your name.
-print("Pi")
-#How do you enter a comment in a Program?
-# you write a hashtag
+1. Print (“Pi”)
 
-print ((3+4)+3)
+2. With # before
+
+3. When you use // it always rounds down to the next integer
+
+4. x=pi print(x)
+
+5. Because a and A is not the same
+
+6. area_of_rectangle
+
+7.  account#  account.number  pi      great.big.variable    total%    #left
+
+8. Because the code is read from top to bottom and it was told to print before what it were printed was mentioned
+
+9. python already knows the value of pi
+
+10. You don’t have to make the x= 3,14 the program knows what pi is
+
+11. To many quotes around x and y
+
+12. 3(x+y) can not be calculated, you have to put in *
+
+13. in front of an question you should alway have input
+
+14. Yes it does not matter how many spaces there are
+
+15. A constant is a type of variable whose value cannot be changed. It is helpful to think of constants as containers that hold information which cannot be changed later.
+
+16. Don’t know
+
+17. /‘ and /“ a quote that allows you to see the ‘ or “ the text
+
+18.   \n next line and /“ double quote
+
+19. Yes
+
+20. There is missing a set of () so the 3 + 4+ 5’s amount is diveted in 3
+
+21.    Python operator is a symbol that performs an operation on one or more operands (+, -, / etc.)
+
+22. 3
+
+23. user_name = input("Enter your name:”)
+
+value = int(input("Enter your age"))
--- week1/lab.py
+++ week1/lab.py
@@ -1,1 +1,18 @@
-#Hello test
+farenheit = input("enter farenheit temperature")
+celcius = (int(farenheit) - 32) * (5/9)
+print("Celcius", celcius)
+
+h = input ("Enter the height of the trapezoid: ")
+x2 = input ("Enter the length of the bottom base: ")
+x1 = input ("Enter the length of the top base: ")
+a = 1/2*(int(x1)+int(x2))*int(h)
+print("Area of a trapezoid:", a)
+
+
+r =  input("enter the radius of the circle: ")
+a = pi*float(r*float(r))
+
+
+r =  input("enter the radius of the circle: ")
+a = pi*float(r)*float(r)
+print("the area of the cirle is: ", a)
